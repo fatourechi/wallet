c4edcdb2bfe30b689a9097494ef080abeee3a9cc
Ramin Fatourechi <r.fatourechi@gmail.com>
2024-02-24 11:28:47 +0330

    fix typo in user service

--- userservice/app/db/service.py
+++ userservice/app/db/service.py
@@ -18,7 +18,7 @@ class UserService:
         wallet_creation_response = requests.post("http://walletservice/wallets/", json=wallet_data)
 
         if wallet_creation_response.status_code == 200:
-            return user, "ok"
+            return user, ""
         else:
             self.repository.delete_user_by_id(user.id) # delete created user because wallet creation failed
             return None, "Failed to create wallet for user"
@@ -30,14 +30,14 @@ class UserService:
         user = self.repository.get_user_by_id(user_id)
         if not user:
             return user, "User not found"
-        return user, "ok"
+        return user, ""
         
     
     def get_user_by_mobile_number(self, mobile_number: str):
         user = self.repository.get_user_by_mobile_number(mobile_number)
         if not user:
             return user, "User not found"
-        return user, "ok"
+        return user, ""
     
     def delete_user(self, user_id: int):
         return self.repository.delete_user_by_id(user_id)
@@ -45,14 +45,14 @@ class UserService:
     def update_user(self, user_id: int, user: schemas.UserUpdate):
         try:
             user = self.repository.update_user_by_user_id(user_id, user)
-            return user, "ok"
+            return user, ""
         except Exception as e:
             return None, str(e)
     
     def update_user_by_mobile_number(self, mobile_number: str, user: schemas.UserUpdate):
         try:
             user = self.repository.update_user_by_mobile_number(mobile_number, user)
-            return user, "ok"
+            return user, ""
         except Exception as e:
             return None, str(e)
             
